0f381f62b6993a47e33fcdaabd2c35d68cb93600
user601 <artemlisicin55@gmail.com>
2021-06-02 17:59:26 +0300

    add section articls design and video popup partners logo

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,9 @@ function scripts(){
 
   return src(['node_modules/jquery/dist/jquery.js',
    'node_modules/slick-carousel/slick/slick.js',
-    'app/js/main.js'])
+   'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'app/js/main.js'
+  ])
 
   .pipe(concat('main.min.js'))
   .pipe(uglify())

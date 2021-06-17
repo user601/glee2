30b4c0a38f22269d287242341a76906c958a7439
user601 <artemlisicin55@gmail.com>
2021-06-17 11:49:00 +0300

    added product list 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,30 @@
 $(function(){
+
+  $('.category-price__input').ionRangeSlider({
+    type: "doule", prefix: "$",
+    onStart: function (data) {
+      
+      $('.category-price__from').text(data.from)
+      $('.category-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.category-price__from').text(data.from);
+      $('.category-price__to').text(data.to);
+  },  
+  });
+
+
 $('.top-slider__inner').slick({
   dots:true,
   arrows:false
-  
-
 });
+
+$(".star").rateYo({
+  starWidth: "11px",
+  normalFill: "#d6d6d6",
+  ratedFill: "#ffcc00",
+  readOnly: true
+  
+});
+
 }); 
--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,8 @@ function scripts(){
   return src(['node_modules/jquery/dist/jquery.js',
    'node_modules/slick-carousel/slick/slick.js',
    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+   'node_modules/rateyo/src/jquery.rateyo.js',
+   'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
     'app/js/main.js'
   ])
 

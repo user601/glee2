c10d08cc50e1ca3f187e1abaa18f01b39a78b9b3
user601 <artemlisicin55@gmail.com>
2021-06-25 01:23:58 +0300

    added contacts

--- app/js/main.js
+++ app/js/main.js
@@ -80,4 +80,13 @@ $(".star").rateYo({
   
 });
 
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
 }); 
